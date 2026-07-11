8bb128d5b067fa032d628e4c1c6b485b715d5ed3
Ibrahima samake <115491419+ibrahimasamake@users.noreply.github.com>
2026-07-11 16:23:21 +0530

    init

---
+++ Python/11-07-2026/loops/condition.py
@@ -0,0 +1,10 @@
+cpu_usage = 75  # Example CPU usage percentage
+if cpu_usage > 80:
+    print("Warning: High CPU usage!")
+elif(cpu_usage > 50):
+    print("CPU usage is moderate.")
+else:
+    print("CPU usage is within normal limits.")
+
+
+    
