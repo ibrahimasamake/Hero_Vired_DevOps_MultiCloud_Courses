1a0b8cf9793f0e86bfe014834bbff86b34d78320
Ibrahima samake <115491419+ibrahimasamake@users.noreply.github.com>
2026-07-11 17:03:47 +0530

    end sessionn

--- Python/11-07-2026/loops/firstPython.py
+++
@@ -1,8 +0,0 @@
-print("hello world")
-"""
-im learning python to get new skills and knowledge in programming. Python is a versatile language that can be used for web development, data analysis, artificial intelligence, and more. By learning Python, I hope to enhance my problem-solving abilities and open up new career opportunities in the tech industry. 
-
-
-
-"""
-print("Im learning python")
